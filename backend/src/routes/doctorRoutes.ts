29f75ef184d9f61b4ce4b0ab46efa91da61bde32
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-06-17 12:32:04 +0530

    feat: add doctor routes

--- backend/src/routes/doctorRoutes.ts
+++ backend/src/routes/doctorRoutes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { saveDoctor, getAllDoctors } from "../controller/doctorController";
+import {
+  saveDoctor,
+  getAllDoctors,
+  getDoctorById,
+  updateDoctor,
+  deleteDoctor,
+} from "../controller/doctorController";
 
 const router = Router();
 
 router.post("/save", saveDoctor);
 router.get("/all", getAllDoctors);
+router.get("/:id", getDoctorById);
+router.get("/:id", updateDoctor);
+router.delete("/:id", deleteDoctor);
 
 export default router;
